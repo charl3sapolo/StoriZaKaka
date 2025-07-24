f0ed58840f65e1a87726ae515d4e86f4b8668e3c
Sarafina Adriano <your.email@example.com>
2025-07-25 00:41:41 +0300

    Tx

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('discover/', views.discover, name='discover'),
+    path('api/trending/<str:media_type>/', views.fetch_trending, name='fetch_trending'),
 ]
