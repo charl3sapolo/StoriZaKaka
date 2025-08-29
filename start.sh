677836be42f6e6c985c2f9f1c89c519d3ae1493d
Sarafina Adriano <132925615+adrianofina@users.noreply.github.com>
2025-08-29 20:11:17 +0300

    wqw

--- start.sh
+++ start.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-# Make sure pip and wheel are up-to-date
+# Upgrade pip, setuptools, wheel
 python -m pip install --upgrade pip setuptools wheel
 
-# Ensure gunicorn is installed in runtime
-python -m pip install gunicorn==21.2.0
+# Install all dependencies using precompiled binaries to avoid Pillow build errors
+python -m pip install --only-binary :all: -r requirements.txt
 
-# Install all dependencies
-python -m pip install -r requirements.txt
-
-
-# Run the Django app
+# Start the Django app with Gunicorn
 python -m gunicorn movierecommender.wsgi:application --bind 0.0.0.0:$PORT
