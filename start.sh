17fac2604acfb7981b62f63fbea69e7a6a6a6716
Sarafina Adriano <132925615+adrianofina@users.noreply.github.com>
2025-08-29 20:01:15 +0300

    w

--- start.sh
+++ start.sh
@@ -5,5 +5,9 @@ python -m pip install --upgrade pip setuptools wheel
 # Ensure gunicorn is installed in runtime
 python -m pip install gunicorn==21.2.0
 
+# Install all dependencies
+python -m pip install -r requirements.txt
+
+
 # Run the Django app
 python -m gunicorn movierecommender.wsgi:application --bind 0.0.0.0:$PORT
