e7234a5a7a9ed5c67e650921bf16b5cd56fbdbcb
Sarafina Adriano <132925615+adrianofina@users.noreply.github.com>
2025-09-12 17:28:47 +0300

    bin

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Upgrade pip, setuptools, wheel
-python -m pip install --upgrade pip setuptools wheel
-
-# Install all dependencies using precompiled binaries to avoid Pillow build errors
-python -m pip install --only-binary :all: -r requirements.txt
-
-# Start the Django app with Gunicorn
-python -m gunicorn movierecommender.wsgi:application --bind 0.0.0.0:$PORT
